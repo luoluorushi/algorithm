a50decdc37918a0616c76079409be89e9408544c
luoluorushi <591034303@qq.com>
2018-07-17 19:45:56 +0800

    while

--- add_link.cpp
+++ add_link.cpp
@@ -9,7 +9,7 @@ public:
         int up = 0;
         ListNode* head_node = new ListNode(0);
         ListNode* pre_node=head_node;
-        do {
+        while (l1 != nullptr && l2 != nullptr) {
             int val = l1->val + l2->val + up;
             int node_val = val % 10;
             up = val / 10;
@@ -17,34 +17,28 @@ public:
             l2 = l2->next;
             pre_node->next = new ListNode(node_val);
             pre_node = pre_node->next;
-            if(l1 == nullptr || l2 == nullptr)
-                break;
-        } while(true);
+        };
         if(l1 == nullptr && l2 == nullptr) {
         }
         else if(l1 == nullptr) {
-            do {
+            while(l2 != nullptr) {
                 int val = l2->val + up;
                 int node_val = val % 10;
                 up = val/10;
                 l2 = l2->next;
                 pre_node->next = new ListNode(node_val);
                 pre_node = pre_node->next;
-                if(l2 == nullptr)
-                    break;
-            } while(true);
+            };
         }
         else {
-            do {
+            while(l1 != nullptr) {
                 int val = l1->val + up;
                 int node_val = val % 10;
                 up = val/10;
                 l1 = l1->next;
                 pre_node->next = new ListNode(node_val);
                 pre_node = pre_node->next;
-                if(l1 == nullptr)
-                    break;
-            } while(true);
+            };
         }
          if(up != 0) {
                 pre_node->next = new ListNode(up);
