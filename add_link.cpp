93d2637de63850c866b7b7999dc2a0d97f54b364
luoluorushi <591034303@qq.com>
2018-07-17 22:57:27 +0800

    carry

--- add_link.cpp
+++ add_link.cpp
@@ -6,13 +6,13 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int up = 0;
+        int carry = 0;
         ListNode* head_node = new ListNode(0);
         ListNode* pre_node=head_node;
         while (l1 != nullptr && l2 != nullptr) {
-            int val = l1->val + l2->val + up;
-            int node_val = val % 10;
-            up = val / 10;
+            carry = l1->val + l2->val + carry;
+            int node_val = carry % 10;
+            carry = carry / 10;
             l1 = l1->next;
             l2 = l2->next;
             pre_node->next = new ListNode(node_val);
@@ -22,9 +22,9 @@ public:
         }
         else if(l1 == nullptr) {
             while(l2 != nullptr) {
-                int val = l2->val + up;
-                int node_val = val % 10;
-                up = val/10;
+                carry = l2->val + carry;
+                int node_val = carry % 10;
+                carry = carry/10;
                 l2 = l2->next;
                 pre_node->next = new ListNode(node_val);
                 pre_node = pre_node->next;
@@ -32,16 +32,16 @@ public:
         }
         else {
             while(l1 != nullptr) {
-                int val = l1->val + up;
-                int node_val = val % 10;
-                up = val/10;
+                carry = l1->val + carry;
+                int node_val =  carry % 10;
+                carry = carry/10;
                 l1 = l1->next;
                 pre_node->next = new ListNode(node_val);
                 pre_node = pre_node->next;
             };
         }
-         if(up != 0) {
-                pre_node->next = new ListNode(up);
+         if(carry != 0) {
+                pre_node->next = new ListNode(carry);
         }
 
         return head_node->next;
